b2667ac1ad9d8c295f9dadf238a023ec0f69c085
Rajib Teminos <rajib.ganguly@teminos.com>
2024-05-03 12:56:07 +0530

    fix unknown route and without login accessess restricts

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Dashboard from './pages/Dashboard';
 import AddDepartment from './pages/AddDepartment';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import Departments from './pages/Departments';
 import Meetings from './pages/Meetings';
 import Reports from './pages/Reports';
@@ -14,19 +15,21 @@ import AddTask from './pages/AddTasks';
 
 function App() {
   // const [ isLoggedIn, setIsLoggedIn] = useState({login: false})
-
+  const token = sessionStorage.getItem('token');
+  const isLogin = (token) ? true: false;
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LogIn />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/departments" element={<Departments />} />
-        <Route path="/meetings" element={<Meetings />} />
-        <Route path="/tasks" element={<Tasks />} />
-        <Route path="/reports" element={<Reports />} />   
-        <Route path="/add_department" element={<AddDepartment />} />  
-        <Route path="/addnewmeetings" element={<AddNewMeeting />} /> 
-        <Route path="/addtasks" element={<AddTask />} />          
+        <Route path="/dashboard" element={!isLogin ? <Navigate to="/" /> : <Dashboard />} />
+        <Route path="/departments" element={!isLogin ? <Navigate to="/" /> : <Departments />} />
+        <Route path="/meetings" element={!isLogin ? <Navigate to="/" /> : <Meetings />} />
+        <Route path="/tasks" element={!isLogin ? <Navigate to="/" /> : <Tasks />} />
+        <Route path="/reports" element={!isLogin ? <Navigate to="/" /> : <Reports />} />   
+        <Route path="/add_department" element={!isLogin ? <Navigate to="/" /> : <AddDepartment />} />  
+        <Route path="/addnewmeetings" element={!isLogin ? <Navigate to="/" /> : <AddNewMeeting />} /> 
+        <Route path="/addtasks" element={!isLogin ? <Navigate to="/" /> : <AddTask />} />  
+        <Route path="*" element={<Navigate to="/" />} />        
         {/* Add more routes as needed */}
       </Routes>
     </Router>
