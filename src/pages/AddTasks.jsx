16f1b20eae644691a4d57663c1bb6a054baf42bc
Raj Koley <133200537+raj-dosen@users.noreply.github.com>
2024-05-24 15:06:43 +0530

    update

--- src/pages/AddTasks.jsx
+++ src/pages/AddTasks.jsx
@@ -343,7 +343,7 @@ export default function AddTasks() {
 
     return (
         <ThemeProvider theme={defaultTheme}>
-            <Box sx={{ display: "flex" }}>
+            <Box display={'flex'}>
                 <CssBaseline />
                 <AppBar position="absolute" open={open}>
                     <Header props={open} onOutput={handleOutput} />
