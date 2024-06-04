ad88c6f491a826e4b00b5fe39e303f46ad8e7724
Dipan <dipanadhikary1706@gmail.com>
2024-06-04 12:49:57 +0530

    change 1

--- src/pages/Tasks.js
+++ src/pages/Tasks.js
@@ -151,21 +151,29 @@ export default function Tasks() {
     { text: "Sub Task", dataField: 'subtask' },
     { text: "Operations", dataField: 'taskoperation' }
   ];
-
+  
   let filteredData = allTaskListsData; // 10
   // department filter
-  if (selectedDept) {
-    if (selectedDept === 'ALL') {
-      filteredData = allTaskListsData;
-    } else {
+  if(selectedDept){
+    if(selectedDept === 'ALL'){
+      if(selectedStatus === 'ALL'){
+        filteredData = allTaskListsData;
+      }else{
+        filteredData = filteredData.filter(task => task.status === selectedStatus);
+      }
+    }else{
       filteredData = filteredData.filter(task => task.department?.[0]?.dep_id === selectedDept);
     }
   }
   // status filter
-  if (selectedStatus) {
-    if (selectedStatus === 'ALL') {
-      filteredData = allTaskListsData;
-    } else {
+  if(selectedStatus){
+    if(selectedStatus === 'ALL'){
+      if(selectedDept === 'ALL'){
+        filteredData = allTaskListsData;
+      }else{
+        filteredData = filteredData.filter(task => task.department?.[0]?.dep_id === selectedDept);
+      }
+    }else{
       filteredData = filteredData.filter(task => task.status === selectedStatus);
     }
   }
