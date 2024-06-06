fbd8378eba7e49e62eb490ddba8f845450c66fa1
Dipan <dipanadhikary1706@gmail.com>
2024-06-06 18:07:41 +0530

    null checking

--- src/pages/common.js
+++ src/pages/common.js
@@ -228,7 +228,7 @@ export const getFileNameFromUrl = (url) => {
 };
 
 export const capitalizeFirstLetter = (str) => {
-  return str.charAt(0).toUpperCase() + str.slice(1);
+  return str?.charAt(0).toUpperCase() + str?.slice(1);
 };
 
 export const formatVerifiedStatus = (admin_verified_status) => {
